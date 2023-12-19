448c9859ca0532aee71ae0b69d590c753d9466a1
MM20 <15646950+MM2-0@users.noreply.github.com>
2023-12-19 17:43:27 +0100

    Version 1.0.0-beta01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0-beta01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
